912b2a49d7761581f6da8a114d5ebeee8c4bb2fd
songzai <songzai@ibantang.com>
2023-04-26 23:35:29 +0800

    a

--- components/Header.js
+++ components/Header.js
@@ -110,7 +110,8 @@ export default function Header ({ navBarTitle, fullWidth }) {
               alt={BLOG.title}
               onError={() => setFavicon(true)}
             />
-            
+          </Link>
+          <Link href="/" aria-label={BLOG.title}>
             <HeaderName
               ref={titleRef}
               siteTitle={BLOG.title}
